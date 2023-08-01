1280004167d70fad72c7b73e803837921a093acb
Stefan Roets <stefanroetscode@gmail.com>
2023-08-01 10:56:19 +0200

    board fixed

--- app.js
+++ app.js
@@ -13,14 +13,26 @@ const startPieces = [
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
     rook, knight, bishop, queen, king, bishop, knight, rook
 ]
-
 function createBoard() {
     startPieces.forEach((startPieces, i) => {
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPieces
         square.setAttribute('square-id', i)
-        square.classList.add('beige')
+        //square.classList.add('beige')
+        const row = Math.floor((63 - i) / 8) + 1
+        if ( row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? "beige" : "brown")
+        } else {
+            square.classList.add(i % 2 === 0 ? "brown" : "beige")
+        }
+
+        if ( i <= 15) {
+            square.firstChild.firstChild.classList.add('black')
+        }
+        if ( i >= 48) {
+            square.firstChild.firstChild.classList.add('white')
+        }
         gameBoard.append(square)
     })
 }
